0ef8b3fac796d1d990bb7be186f8885160771e31
Aditya Upadhyay <adiupd123@gmail.com>
2022-07-31 12:15:32 +0530

    Court Counter App
    Made the internal functionality to increase the scores of both teams and reset their scores.

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -3,12 +3,63 @@ package com.example.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int teamAScore = 0, teamBScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void threePointsTeamA(View view){
+        teamAScore = teamAScore + 3;
+        displayForTeamA(teamAScore);
+    }
+
+    public void twoPointsTeamA(View view){
+        teamAScore = teamAScore + 2;
+        displayForTeamA(teamAScore);
+    }
+
+    public void freeThrowTeamA(View view){
+        teamAScore = teamAScore + 1;
+        displayForTeamA(teamAScore);
+    }
+
+    public void threeePointsTeamB(View view){
+        teamBScore = teamBScore + 3;
+        displayForTeamB(teamBScore);
+    }
+
+    public void  twoPointsTeamB(View view){
+        teamBScore = teamBScore + 2;
+        displayForTeamB(teamBScore);
+    }
+
+    public void freeThrowTeamB(View view){
+        teamBScore = teamBScore + 1;
+        displayForTeamB(teamBScore);
+    }
+
+    public void reset(View view){
+        teamAScore = 0;
+        teamBScore = 0;
+        displayForTeamA(teamAScore);
+        displayForTeamB(teamBScore);
+    }
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayForTeamB(int score){
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(""+score);
     }
 }
